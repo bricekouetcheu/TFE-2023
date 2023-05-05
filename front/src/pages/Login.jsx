ed66bfde9df805fcf06d5d5aca2bcee1ab3e1249
bricekouetcheu <brcronld@gmail.com>
2023-05-06 00:08:59 +0200

    login form page ok

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import logo from "../assets/logo.png";
 
 const Login = () => {
     return (
-        <div className=''>
-            
-        </div>
+        <div className='login'>
+            <div className='login-form-navbar'>
+                <img className = 'login-form-logo' src={logo} alt=""/>
+
+             </div>
+     
+            <form className='login-form'>
+                <h2>Login</h2>
+                <div className='login-form-content'>
+                    <input type="email" id="email" name="email" required placeholder="Email..."/>
+                    <input type="password" id="password" name="password" required placeholder="Password..."/>
+
+
+                </div>
+          <div className='login-form-checkbox'>
+             <p> <a href=""> Forgot password?</a></p>
+                      
+
+                 
+          </div>
+               
+
+       
+        
+
+          <button className='login-form-btn'>Login</button>
+          <div className='login-form-signin'>
+          <p>you don't have an account yet?<a href=""> SIGN UP</a></p>
+
+          </div>
+          
+         
+
+
+        </form>
+
+      </div>
+
     );
 };
 

4951ecb00a36e2fc9dac6c6305455b3173025b8e
bricekouetcheu <brcronld@gmail.com>
2023-06-22 11:51:59 +0200

    set up environnement test

--- front/src/__test__/Login.test.js
+++ front/src/__test__/Login.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import Login from './pages/Login';
+import Login from '../pages/Login';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 jest.mock('axios'); // Mock d'axios pour simuler les appels réseau
 
 describe('Login component', () => {
   it('renders correctly', () => {
-    render(<Login />);
+    render(
+        <Router>
+            <Login />
+        </Router>
+    );
     // Ajoute ici tes assertions pour vérifier le rendu du composant
   });
 
